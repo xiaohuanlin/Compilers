0e95ae55165058693b28cddf6e05d8f7a67a58d4
xhl <123971348@qq.com>
2019-03-28 19:47:04 +0800

    add NFA

--- model/state_machine.py
+++ model/state_machine.py
@@ -1,4 +1,5 @@
-from typing import List, NewType
+from typing import List, Sequence
+from itertools import chain
 
 
 class FARule:
@@ -9,13 +10,15 @@ class FARule:
         self.next_state = next_state
 
     def applies_to(self, state, character):
+        # indicate if it can accept this state and character
         return self.state == state and self.character == character
 
     def follow(self):
+        # return the next transfer state
         return self.next_state
 
     def __repr__(self):
-        return f'{self.__class__.__name__}: {self.state} -- {self.character} --> {self.next_state}'
+        return f'{self.__class__.__name__}: {self.state} / {self.character} --> {self.next_state}'
 
 
 class DFARuleBook:
@@ -24,11 +27,13 @@ class DFARuleBook:
         self.rules = rules
 
     def rule_for(self, state, character):
+        # search for and return the right rule
         for rule in self.rules:
             if rule.applies_to(state, character):
                 return rule
 
     def next_state(self, state, character):
+        # transfer to next state
         return self.rule_for(state, character).follow()
 
 
@@ -39,9 +44,11 @@ class DFA:
         self.rulebook = rulebook
 
     def accepting(self):
+        # determine if it can be accepted
         return self.current_state in self.accept_states
 
     def read_character(self, character):
+        # transfer to next state by using this character
         self.current_state = self.rulebook.next_state(self.current_state, character)
 
     def read_string(self, string):
@@ -56,7 +63,62 @@ class DFAFactory:
         self.rulebook = rulebook
 
     def get_dfa(self):
+        # generate a new dfa
         return DFA(self.start_state, self.accept_states, self.rulebook)
+
+    def accept(self, string):
+        dfa = self.get_dfa()
+        dfa.read_string(string)
+        return dfa.accepting()
+
+
+class NFARuleBook:
+    def __init__(self, rules: List[FARule]):
+        self.rules = rules
+
+    def rule_for(self, state, character) -> Sequence:
+        # collect all rules meet the state and character
+        return [rule for rule in self.rules if rule.applies_to(state, character)]
+
+    def follow_rules_for(self, state, character) -> Sequence:
+        # gather all states after one state transfer to next state
+        return [rule.follow() for rule in self.rule_for(state, character)]
+
+    def next_states(self, states, character):
+        # return all possible states
+        return set(_state
+                   for state in states
+                   for _state in self.follow_rules_for(state, character))
+
+
+class NFA:
+    def __init__(self, current_states: set, accept_states: set, rulebook: NFARuleBook):
+        self.current_states = current_states
+        self.accept_states = accept_states
+        self.rulebook = rulebook
+
+    def accepting(self):
+        # determine if it can be accepted
+        return self.current_states & self.accept_states
+
+    def read_character(self, character):
+        # transfer to next state by using this character
+        self.current_states = self.rulebook.next_states(self.current_states, character)
+
+    def read_string(self, string):
+        for s in string:
+            self.read_character(s)
+
+
+class NFAFactory:
+    def __init__(self, start_state: int, accept_states: List[int], rulebook: NFARuleBook):
+        self.start_state = start_state
+        self.accept_states = accept_states
+        self.rulebook = rulebook
+
+    def get_dfa(self):
+        # generate a new nfa
+        return NFA({self.start_state}, set(self.accept_states), self.rulebook)
 
     def accept(self, string):
         dfa = self.get_dfa()
--- test/test_state_machine.py
+++ test/test_state_machine.py
@@ -1,4 +1,4 @@
-from model.state_machine import DFARuleBook, FARule, DFA, DFAFactory
+from model.state_machine import DFARuleBook, FARule, DFA, DFAFactory, NFARuleBook, NFA, NFAFactory
 from unittest import TestCase
 
 
@@ -48,4 +48,46 @@ class TestStateMachine(TestCase):
         self.assertFalse(dfa_factory.accept('baa'))
         self.assertTrue(dfa_factory.accept('baba'))
 
+    def test_nfa(self):
+        rulebook = NFARuleBook(
+            [
+                FARule(1, 'a', 1),
+                FARule(1, 'b', 1),
+                FARule(1, 'b', 2),
 
+                FARule(2, 'a', 3),
+                FARule(2, 'b', 3),
+
+                FARule(3, 'a', 4),
+                FARule(3, 'b', 4),
+            ]
+        )
+        self.assertEqual(rulebook.next_states([1], 'b'), {1, 2})
+        self.assertEqual(rulebook.next_states([1, 2], 'a'), {1, 3})
+        self.assertEqual(rulebook.next_states([1, 3], 'b'), {1, 2, 4})
+
+        # test read_character
+        nfa = NFA({1}, {4}, rulebook)
+        self.assertFalse(nfa.accepting())
+
+        nfa.read_character('b')
+        self.assertFalse(nfa.accepting())
+
+        nfa.read_character('a')
+        self.assertFalse(nfa.accepting())
+
+        nfa.read_character('b')
+        self.assertTrue(nfa.accepting())
+
+        nfa = NFA({1}, {4}, rulebook)
+        self.assertFalse(nfa.accepting())
+
+        # test read_string
+        nfa.read_string('bbbbb')
+        self.assertTrue(nfa.accepting())
+
+        # test NFAFactory
+        nfa_factory = NFAFactory(1, [4], rulebook)
+        self.assertTrue(nfa_factory.accept('bab'))
+        self.assertTrue(nfa_factory.accept('bbbbb'))
+        self.assertFalse(nfa_factory.accept('bbabb'))
